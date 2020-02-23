91c7a3c3036840c2c3f901311d2b9ca98f19a584
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-23 21:37:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -14,12 +14,12 @@ require 'pry'
  
 
 def key_for_min_value(name_hash)
-  lowest_value = nil
+  lowest_value = 0
   lowest_key = nil
 
 end 
   name_hash.each do |key, value|
-    if lowest_key == nil || value < lowest_value
+    if lowest_key == 0 || value < lowest_value
       lowest_value = value
       lowest_key = key
     end
